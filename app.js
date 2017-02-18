d4d26cb40e8d7f00ff45029ec4ac0e2475e3d1b9
Sven Bernadus Koopmans <svenkoopmans808@gmail.com>
2017-02-18 16:50:06 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,91 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var score = 0;
+var life = 3;
+function preload() {
+	game.load.image('sky', 'assets/sky,png');
+	game.load.image('ground', 'assets/platform.png');
+	game.load.image('star', 'assets/star.png');
+	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+	game.load.spritesheet('evils', 'assets/baddie.png', 32, 32);
+
+}
+
+function create() {
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	game.add.sprite(0, 0, 'sky');
+
+	platforms = game.add.physicsGroup();
+	platforms.enableBody = true;
+
+	var ground = platforms.create(0, game.world.height - 50, 'ground');
+	ground.scale.setTo(2,2);
+	ground.body.immovable = true;
+
+	var ledge = platforms.create(400, 400, 'ground');
+	ledge.body.immovable = true;
+	var ledge = platforms.create(-150, 250, 'ground');
+	ledge.body.immovable = true;
+
+	player = game.add.sprite(32, game.world.height - 220, 'dude');
+	player.animations.add('left', [0,1,2,3], 10, true);
+	player.animations.add('right', [5,6,7,8], 10, true);
+	game.physics,arcade.enable(player);
+	player.body.bounce.y = 0.2;
+	player.body.gravity.y = 300;
+	player.body.collideWorldBounds = true;
+
+	enemy1 = game.add.sprite(750, 20, 'evils');
+	enemy1.animations.add('left', [0,1], 10, true)
+	enemy1.animations.add('right', [2,3], 10, true)
+	enemy1.body.bounce.y = 0.2;
+	enemy1.body.gravity.y = 500;
+	player.body.collideWorldBounds = true;
+
+	enemy2 = game.add.sprite(10, 20, 'evils');
+	enemy2.animations.add('left', [0,1], 10, true)
+	enemy2.animations.add('right', [2,3], 10, true)
+	enemy2.body.bounce.y = 0.2;
+	enemy2.body.gravity.y = 500;
+	player.body.collideWorldBounds = true;
+
+	enemy3 = game.add.sprite(200, 20, 'evils');
+	enemy3.animations.add('left', [0,1], 10, true)
+	enemy3.animations.add('right', [2,3], 10, true)
+	enemy3.body.bounce.y = 0.2;
+	enemy3.body.gravity.y = 500;
+	player.body.collideWorldBounds = true;
+
+	cursors = gmae.input.keyboard.createCursorKeys();
+
+	stars = game.add.physicsGroup();
+	stars.enableBody = true;
+	for( var i = 0; i < 12; i++) {
+		var star = stars.create(i * 70, 0, 'star');
+		star.body.gravity.y = 200;
+		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+
+	}
+
+	var style = {font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV:"middle"};
+	scoreLabel = gmae.add.text(-60, 0, "Your Score is: ", style);
+	scoreText = game.add.text(70, 0, score, style);
+	scoreLabel.setShadow(3, 3, "rbga(0, 0, 0, 0.5") , 2);
+	scoreText.setShadow(3, 3, "rbga(0, 0, 0, 0.5") , 2);
+	scoreLabel.setTextBounds(0, 520, 800, 100);
+	scoreText.setTextBounds(0, 520, 800, 100);
+
+	lifeLabel = gmae.add.text(-300, 0, "Lives: ", style);
+	lifeText = game.add.text(-240, 0, life, style);
+	lfieLabel.setShadow(3, 3, "rbga(0, 0, 0, 0.5") , 2);
+	lifeText.setShadow(3, 3, "rbga(0, 0, 0, 0.5") , 2);
+	lfieLabel.setTextBounds(0, 0, 800, 100);
+	lifeText.setTextBounds(0, 0, 800, 100);
+	
+
+}
+
+
+}
+
+function update() {
+}
